de155742ca4dfbc207c70505eec5e63be94fb681
DavidFranco135 <davidbhmg147@gmail.com>
2026-07-17 03:24:33 -0300

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,7 +13,11 @@ export {
   listPresencialSessions,
   bookPresencialSession,
   confirmCheckin,
+  listSessionAttendees,
 } from "./presencial";
 export { uploadImage } from "./imgbb";
 export { applyScholarship, listScholarshipApplications } from "./scholarship";
 export { getSiteContent, updateSiteContent } from "./siteContent";
+export { getStudentProgress, markLessonComplete } from "./progress";
+export { listStudents, listTransactions, getOverviewStats } from "./admin";
+export { registerLead, listLeads } from "./leads";
